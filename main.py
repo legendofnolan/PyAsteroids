ef959c9473f047a48a8c4ba9586d6030058197b1
legendofnolan <legendofnolan@gmail.com>
2025-07-20 16:23:23 -0700

    finished player

--- main.py
+++ main.py
@@ -3,9 +3,17 @@
 # throught this file
 import pygame
 from constants import *
-
+from player import Player
+#use this command to get uv to run the right packages source .venv/bin/activate
 def main():
     pygame.init()
+    PyClock = pygame.time.Clock()
+    dt = 0 #I assume deltatime
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    player = Player( (SCREEN_WIDTH / 2), (SCREEN_HEIGHT /2) )
+
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {720}")
@@ -17,8 +25,19 @@ def main():
             if event.type == pygame.QUIT:
                 return
 
-        screen.fill((0,0,0))
-        pygame.display.update()
+        #player.update(dt)
+        updatable.update(dt)
+
+        screen.fill
+        #player.draw(screen)
+        #drawable.draw(screen)
+        for each in drawable:
+            each.draw(screen)
+
+        pygame.display.flip()
+        dt = PyClock.tick(60) / 1000
+        #PyClock.tick(60)
+
 
 
 
